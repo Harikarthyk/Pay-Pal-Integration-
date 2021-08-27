b2d1cbc9069a2f7d518444102561818dd46809ac
HariKarthyk <hari.jsmith494@gmail.com>
2021-08-27 12:00:53 +0530

    completed

--- server.js
+++ server.js
@@ -1,64 +1,72 @@
+require('dotenv')
 const express = require("express");
-const bodyParser = require("body-parser");
 const engines = require("consolidate");
 const paypal = require("paypal-rest-sdk");
 
 const app = express();
 
+
 app.engine("ejs", engines.ejs);
 app.set("views", "./views");
 app.set("view engine", "ejs");
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 paypal.configure({
     mode: "sandbox", //sandbox or live
     client_id:
-        "AarUi7DfcZaVwTyuHOE2qMPGP1Gy65T8KNHicseSgSXB-gm_2upRM74fU-MKmslNaHKqNOUsxMrNv9I-",
+        'AbiRj24VH3krWUGCJ8o54JUbQA_CczdQZUC8qQCG0-ukaYTcI4V-Fleap5nOnI6p9mYgQ2mSofvHp6PF',
     client_secret:
-        "EI0ea3hVzYFwuts33i0RUVhxF48woSUSg7lwNbkImLrHpcEYpUmJPRXdf4CXn4kFacsRoZ-62cn9Xe6h"
+        'ENTVFGwF_TASlgTsVPpRhIa7mi6Xkav-YpaO4hfPaY78qlGWsnzD3yW34yvGbtM6Km8peCnMzOE7WpT2'
+});
+
+/**
+ * Test Route
+ */
+app.get("/check", (req, res) => {
+    res.send("Its up and running.");
 });
 
 app.get("/", (req, res) => {
     res.render("index");
 });
 
-app.get("/paypal", (req, res) => {
+app.post("/paypal", (req, res) => {
     var create_payment_json = {
         intent: "sale",
         payer: {
             payment_method: "paypal"
         },
         redirect_urls: {
-            return_url: "http://localhost:3000/success",
-            cancel_url: "http://localhost:3000/cancel"
+            return_url: `${url}success`,
+            cancel_url: `${url}cancel`
         },
         transactions: [
             {
                 item_list: {
-                    items: [
-                        {
-                            name: "item",
-                            sku: "item",
-                            price: "1.00",
-                            currency: "USD",
-                            quantity: 1
-                        }
-                    ]
+                    items: req.body.items
                 },
                 amount: {
                     currency: "USD",
-                    total: "1.00"
+                    total:req.body.total,
+                    details: {
+                        subtotal: req.body.subtotal,
+                        tax: req.body.tax,
+                        shipping: req.body.shipping,
+                        discount: req.body.discount
+                      }
                 },
-                description: "This is the payment description."
+                description: "Organize Me",
+               
             }
         ]
     };
-
+    console.log(create_payment_json)
     paypal.payment.create(create_payment_json, function(error, payment) {
         if (error) {
-            throw error;
+            console.log(error);
+            res.render("failed");
         } else {
             console.log("Create Payment Response");
             console.log(payment);
@@ -67,29 +75,23 @@ app.get("/paypal", (req, res) => {
     });
 });
 
+/**
+ * Redirection url if success
+ */
 app.get("/success", (req, res) => {
-    // res.send("Success");
+    console.log('-----comming')
     var PayerID = req.query.PayerID;
     var paymentId = req.query.paymentId;
     var execute_payment_json = {
         payer_id: PayerID,
-        transactions: [
-            {
-                amount: {
-                    currency: "USD",
-                    total: "1.00"
-                }
-            }
-        ]
     };
-
     paypal.payment.execute(paymentId, execute_payment_json, function(
         error,
         payment
     ) {
         if (error) {
             console.log(error.response);
-            throw error;
+            res.render("failed");
         } else {
             console.log("Get Payment Response");
             console.log(JSON.stringify(payment));
@@ -98,10 +100,19 @@ app.get("/success", (req, res) => {
     });
 });
 
-app.get("cancel", (req, res) => {
+;
+
+/**
+ * Redirection url if cancel
+ */
+app.get("/cancel", (req, res) => {
     res.render("cancel");
 });
-
-app.listen(3000, () => {
-    console.log("Server is running");
+app.get("/failed", (req, res) => {
+    res.render("failed");
 });
+
+const PORT = 5050;
+let url = process.env.NODE_ENV === "production" ? "" : `http://localhost:${PORT}/`
+
+app.listen(PORT, () => console.log(`Server is up and running at ${PORT}`));
